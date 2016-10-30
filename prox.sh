87a989f96075fa1dc3e75e8ebb221c2c9c0d957e
dirkpetersen <petersen@fhcrc.org>
2016-10-30 07:47:03 -0700

    changed prox.sh command

--- prox.sh
+++ prox.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 # command for deploying proxmox container
-prox --mem 8G --disk 8G --cores 8 --no-bootstrap --runlist easybuild.runlist new myhost
+if [[ -z "$1" ]]; then
+  echo "usage: prox.sh <newhostname>"
+  exit 1
+fi
+prox new --mem 8G --disk 8G --cores 8 --no-bootstrap --runlist easybuild.runlist $1
 
