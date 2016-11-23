5f254a031f01b2089288dee3d27b8f4f865f5cfe
dirkpetersen <petersen@fhcrc.org>
2016-11-23 15:08:51 -0800

    fixing package dependencies

--- update-eb-files.sh
+++ update-eb-files.sh
@@ -2,11 +2,11 @@
 
 if [[ -d easybuild/easyconfigs ]]; then
   rsync -av /app/easybuild/ebfiles_repo/ easybuild/easyconfigs/
-  mkdir -p easybuild/easyconfigs/-PATCHES
-  cp  /app/easybuild/fh_easyconfigs/*.patch easybuild/easyconfigs/-PATCHES
+  cp  /app/easybuild/fh_easyconfigs/*.patch easybuild/easyconfigs/
   echo "deleting unwanted easyconfigs ..."
   oldstuff=".*\(2014a\|2014b\|2015a\|2015b\|goolf\|ictce\|iimpi\|ifort\|icc-\|CrayGNU\|iomkl\|gimkl\).*.eb"
   find easybuild/easyconfigs -regex "${oldstuff}"  -exec rm {} \;
+  git add easybuild
 else
   echo "folder easybuild/easyconfigs does not exist."
 fi
