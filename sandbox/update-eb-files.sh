0707aa52b4ddef74c362249ee2217fdddb6243b1
dirkpetersen <petersen@fhcrc.org>
2017-03-22 21:49:45 -0700

    Sandbox March 2017 / 2

--- sandbox/update-eb-files.sh
+++ sandbox/update-eb-files.sh
@@ -7,6 +7,7 @@ if [[ -d easybuild/easyconfigs ]]; then
   oldstuff=".*\(2014a\|2014b\|2015a\|2015b\|goolf\|ictce\|iimpi\|ifort\|icc-\|CrayGNU\|iomkl\|gimkl\).*.eb"
   find easybuild/easyconfigs -regex "${oldstuff}"  -exec rm {} \;
   ./sandbox/osdep.py
+  mv os-dependencies.* ./sandbox
   git add easybuild
 else
   echo "folder easybuild/easyconfigs does not exist. Please switch to the root of the easybuild-life-sciences repos."
