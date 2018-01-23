a0eb3f3548520ae336c4d517690c7347a8271c2b
Ben McGough <bmcgough@gmail.com>
2018-01-23 10:45:27 -0800

    initial commit with initial README

---
+++ sources/download_sources.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# script to download sources too large to fit into a git repo
+curl -o /app/sources/jdk-8u92-linux-x64.tar.gz https://s3-us-west-2.amazonaws.com/ls2-sources/jdk-8u92-linux-x64.tar.gz
