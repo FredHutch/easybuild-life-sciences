52e7aef1d94783c0b9b1cf6d5a9c6edf4903c8d6
fizwit <john@fuzzdog.com>
2018-09-26 14:49:16 -0700

    BBMap module

--- scripts/create_module_list.sh
+++ scripts/create_module_list.sh
@@ -4,6 +4,7 @@
 #
 # Create a list of modules. Filter modules based on module class 'bio', 'chem', 'phys' and
 # 'math'
+# Used to create software Inventory
 
 repo='easybuild-life-sciences'
 
@@ -13,27 +14,37 @@ if [[ ! -z "${PWD##*${repo}*}" ]]; then
     exit 1
 fi
 
+# extract major.minor from OS relase: VERSION="16.04.3 LTS (Xenial Xerus)"
+os_ver=`grep '^VERSION=' /etc/os-release | sed 's/.*="\([0-9]*\.[0-9]*\).*$/\1/'`
+inventory=bio-modules-${os_ver}.md
+
 # remove the $repo from left everything else; add the $repo and the "/docs" dir back
 base_dir=${PWD%${repo}*}${repo}
 docs_dir=${base_dir}/docs
 scripts_dir=${base_dir}/scripts
 
-spider=/app/Lmod/lmod/lmod/libexec/spider
-module_dir=/app/easybuild/modules
+if [[ $os_ver == '16.04' ]]; then
+    spider=/app/lmod/lmod/libexec/spider
+    module_dir=/app/modules
+elif [[ $os_ver == '14.04' ]]; then
+    spider=/app/Lmod/lmod/lmod/libexec/spider
+    module_dir=/app/easybuild/modules
+fi
+
 cd $base_dir
 $spider -o spider-json ${module_dir}/bio:${module_dir}/math | python -mjson.tool >${docs_dir}/modules.json
 
-echo '---' > ${docs_dir}/bio-modules.md
-echo 'layout: post' >> ${docs_dir}/bio-modules.md
-echo 'title: Bio Modules' >> ${docs_dir}/bio-modules.md
-echo 'date: '`date +'%Y-%m-%d'` >> ${docs_dir}/bio-modules.md
-echo '---' >> ${docs_dir}/bio-modules.md
-echo '' >> ${docs_dir}/bio-modules.md 
+echo '---' > ${docs_dir}/${inventory}
+echo 'layout: post' >> ${docs_dir}/${inventory}
+echo 'title: Bio Modules' >> ${docs_dir}/${inventory}
+echo 'date: '`date +'%Y-%m-%d'` >> ${docs_dir}/${inventory}
+echo '---' >> ${docs_dir}/${inventory}
+echo '' >> ${docs_dir}/${inventory} 
 
 python - <<EOF
 import json
 jfile = open('docs/modules.json', 'r')
-outfile = open('docs/bio-modules.md', 'a')
+outfile = open('docs/${inventory}', 'a')
 data = json.load(jfile)
 packages = data.keys()
 slist = sorted(packages)
