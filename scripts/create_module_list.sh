f2fbc25085cff7116674f63d96c7bed65df861ad
fizwit <john@fuzzdog.com>
2018-10-09 13:24:48 -0700

    big bunch of 2018b packages

--- scripts/create_module_list.sh
+++ scripts/create_module_list.sh
@@ -34,36 +34,11 @@ fi
 cd $base_dir
 $spider -o spider-json ${module_dir}/bio:${module_dir}/math | python -mjson.tool >${docs_dir}/modules.json
 
+echo 'start inventory'
 echo '---' > ${docs_dir}/${inventory}
 echo 'layout: post' >> ${docs_dir}/${inventory}
 echo 'title: Bio Modules' >> ${docs_dir}/${inventory}
 echo 'date: '`date +'%Y-%m-%d'` >> ${docs_dir}/${inventory}
 echo '---' >> ${docs_dir}/${inventory}
 echo '' >> ${docs_dir}/${inventory} 
-
-python - <<EOF
-import json
-jfile = open('docs/modules.json', 'r')
-outfile = open('docs/${inventory}', 'a')
-data = json.load(jfile)
-packages = data.keys()
-slist = sorted(packages)
-for p in slist:
-   pac = data[p]
-   for ver in pac.keys():
-       if '-2015' in pac[ver]['full']: 
-            continue
-       descrp = ''
-       url = ''
-       if 'Description' in pac[ver]:
-           text = pac[ver]['Description'].split(' - ')[0]
-           descrp = text.encode('utf8', 'replace')
-       if 'whatis' in pac[ver]:
-           entry = [x for x in pac[ver]['whatis'] if 'Homepage: ' in x]
-           text = entry[0].split('Homepage: ')[1]
-           url = text.encode('utf8', 'replace')
-       outfile.write(' - [' + pac[ver]['full'] + ']')
-       outfile.write('(' + url + ')  ')
-       outfile.write(descrp + '\n')
-EOF
 
