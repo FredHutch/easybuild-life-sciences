2fc97b3cdf2e5ae3f00a8e3d03a14e0fd1cc716e
fizwit <john@fuzzdog.com>
2018-10-11 10:10:13 -0700

    spider2post only post latest version for each package

--- scripts/spider2post.py
+++ scripts/spider2post.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from pprint import pprint
 import json
 import sys
 
@@ -8,18 +9,23 @@ packages = data.keys()
 slist = sorted(packages)
 for p in slist:
    pac = data[p]
-   for ver in pac.keys():
-       if 'fullName' in pac[ver] and '-2015' in pac[ver]['fullName']:
-            continue
-       descrp = ''
-       url = ''
-       if 'Description' in pac[ver]:
-           text = pac[ver]['Description'].split(' - ')[0]
-           descrp = text.encode('utf8', 'replace')
-       if 'whatis' in pac[ver]:
-           entry = [x for x in pac[ver]['whatis'] if 'Homepage: ' in x]
-           text = entry[0].split('Homepage: ')[1]
-           url = text.encode('utf8', 'replace')
-       print(' - [' + pac[ver]['fullName'] + ']')
-       print('(' + url + ')  ')
-       print(descrp + '\n')
+   paths = list(pac.keys())
+   if len(paths) == 1:
+      release = pac[paths[0]]
+   else:
+      vers = sorted(paths)
+      release = pac[vers[-1]]
+   if 'fullName' in release and '-2015' in release['fullName']:
+        continue
+   descrp = ''
+   url = ''
+   if 'Description' in release:
+       text = release['Description'].split(' - ')[0]
+       descrp = text.encode('utf8', 'replace')
+   if 'whatis' in release:
+       entry = [x for x in release['whatis'] if 'Homepage: ' in x]
+       text = entry[0].split('Homepage: ')[1]
+       url = text.encode('utf8', 'replace')
+   print(' - [' + release['fullName'] + ']')
+   print('(' + url + ')  ')
+   print(descrp + '\n')
