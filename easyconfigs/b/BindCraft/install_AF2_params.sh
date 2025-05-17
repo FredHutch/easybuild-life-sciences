f8d3e557a6f78e434611567a3bd75ef92a3d42f9
fizwit <john@fuzzdog.com>
2025-05-17 13:07:11 -0700

    BindCraft update ColabDesign too 1.1.3

---
+++ easyconfigs/b/BindCraft/install_AF2_params.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+################## BindCraft installation script
+SECONDS=0
+
+# set paths needed for installation and check for conda installation
+install_dir=$(pwd)
+
+# AlphaFold2 weights
+echo -e "Downloading AlphaFold2 model weights \n"
+params_dir="${install_dir}/params"
+params_file="${params_dir}/alphafold_params_2022-12-06.tar"
+
+# download AF2 weights
+mkdir -p "${params_dir}" || { echo -e "Error: Failed to create weights directory"; exit 1; }
+wget -O "${params_file}" "https://storage.googleapis.com/alphafold/alphafold_params_2022-12-06.tar" || { echo -e "Error: Failed to download AlphaFold2 weights"; exit 1; }
+[ -s "${params_file}" ] || { echo -e "Error: Could not locate downloaded AlphaFold2 weights"; exit 1; }
+
+# extract AF2 weights
+tar tf "${params_file}" >/dev/null 2>&1 || { echo -e "Error: Corrupt AlphaFold2 weights download"; exit 1; }
+tar -xvf "${params_file}" -C "${params_dir}" || { echo -e "Error: Failed to extract AlphaFold2weights"; exit 1; }
+[ -f "${params_dir}/params_model_5_ptm.npz" ] || { echo -e "Error: Could not locate extracted AlphaFold2 weights"; exit 1; }
+rm "${params_file}" || { echo -e "Warning: Failed to remove AlphaFold2 weights archive"; }
+
+################## finish script
+t=$SECONDS 
+echo -e "Successfully installed AF2 params data!\n"
+echo -e "\n"
+echo -e "Installation took $(($t / 3600)) hours, $((($t / 60) % 60)) minutes and $(($t % 60)) seconds."
